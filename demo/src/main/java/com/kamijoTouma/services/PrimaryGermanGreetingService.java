0199be3d6618e0e384cf3b5f027ad3bede8e51e7
蒋凝哲 <Jiang@Ningzhe-Jiangs-MacBook-Air.local>
2017-10-09 16:23:59 -0400

    lesson 33

--- demo/src/main/java/com/kamijoTouma/services/PrimaryGermanGreetingService.java
+++ demo/src/main/java/com/kamijoTouma/services/PrimaryGermanGreetingService.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en","default"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
 
 	@Override
 	public String sayHello() {
-		return "hello from primary greeting service";
+		return "greeting fuher";
 	}
 
 }
